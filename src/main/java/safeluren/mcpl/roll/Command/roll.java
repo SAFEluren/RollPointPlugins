ad550f22cd47a0a8dd99a36430aff41cc9e2fbae
SAFEluren <3206275107@qq.com>
2022-02-26 22:35:47 +0800

    fix bug: some message no color

--- src/main/java/safeluren/mcpl/roll/Command/roll.java
+++ src/main/java/safeluren/mcpl/roll/Command/roll.java
@@ -22,14 +22,13 @@ public class roll implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { // 命令部分
         Random r = new Random(); // random的包重命名为r
         if (sender instanceof Player) { // sender 是否为玩家
-            //错误消息 : 长度为零
             if (args.length == 1){
                 String i_str = args[0];
                 if (judgeAllNum(i_str)) {
                     int i_num = Integer.parseInt(i_str); // 转化整数
                     if (i_num > 0 & i_num < 214748367) { // 输入数字是否大于等于int上限
                         int num = r.nextInt(i_num) + 1; // 随机一个 1 - i_num的数字
-                        sender.sendMessage(prefix+okay + num); // 随机完了 发消息
+                        sender.sendMessage(color.format(prefix+okay + num)); // 随机完了 发消息
                     } else {
                         sender.sendMessage(color.format(String.format("%s",prefix+error))); // 发给玩家错误消息
                     }
@@ -38,7 +37,7 @@ public class roll implements CommandExecutor {
             }
             sender.sendMessage(color.format(String.format("%s",prefix+error))); // 错误消息 : 非纯数字
         } else {
-            System.out.println(prefix + tips_op); //后台执行提醒仅玩家可用
+            System.out.println(color.format(prefix + tips_op)); //后台执行提醒仅玩家可用
         }
         return true;
     }
